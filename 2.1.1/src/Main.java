ddfc0b0da8b0c9d84d15a4d80b3f8b914ddcb8c5
Stanislav Rakitov <rakitov.s@gmail.com>
2021-10-23 17:26:34 +0300

    2.1.1 done

--- 2.1.1/src/Main.java
+++ 2.1.1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     String[] products = {"Хлеб", "Яблоки", "Молоко"};
@@ -6,7 +8,36 @@ public class Main {
     for (int i = 0; i < products.length; i++) {
       System.out.printf("%d. %s %d руб/шт%n", i + 1, products[i], prices[i]);
     }
+    int productNumber;
+    int productCount;
+    int[] cart = new int[products.length];
+    Scanner scanner = new Scanner(System.in);
 
-//    System.out.printf("");
+    while (true) {
+      System.out.println("Выберите товар и количество или введите `end`");
+      String line = scanner.nextLine();
+      if (line.equals("end") || line.equals("")) {
+        break;
+      }
+      String[] split = line.split(" ");
+      productNumber = Integer.parseInt(split[0]) - 1;
+      productCount = Integer.parseInt(split[1]);
+      cart[productNumber] += productCount;
+    }
+
+    System.out.println("Ваша корзина:");
+    System.out.println("Наименование товара  Количество  Цена/за.ед  Общая стоимость");
+
+    int totalPrice = 0;
+
+    for (int i = 0; i < products.length; i++) {
+
+      if (cart[i] != 0) {
+        int current = prices[i] * cart[i];
+        System.out.printf("%-20s %-11d %-11d %d %n", products[i], cart[i], prices[i], current);
+        totalPrice += current;
+      }
+    }
+    System.out.printf("%38s %9d", "Итого", totalPrice);
   }
 }
