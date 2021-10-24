e90f59c7b67ad45d21f17a73f77016f7bb8b59d8
Stanislav Rakitov <prosperousrf@gmail.com>
2021-10-24 13:53:03 +0300

    2.2.1 init

--- 2.2.1/src/Main.java
+++ 2.2.1/src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Random;
+
 public class Main {
   public static void main(String[] args) {
     int SIZE = 8;
     int[][] colors = new int[SIZE][SIZE];
+
+    Random random = new Random();
+    for (int i = 0; i< SIZE; i++) {
+      for (int j = 0; j< SIZE; j++) {
+        // для случайных значений воспользуемся готовым решением из библиотеки java.util.Random
+        colors[i][j] = random.nextInt(256);
+      }
+    }
 
 
   }
