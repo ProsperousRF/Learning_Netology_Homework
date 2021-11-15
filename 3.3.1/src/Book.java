85ff1b2403623c893acfdc4fc87ecc4cacbbd1c4
Stanislav Rakitov <prosperousrf@gmail.com>
2021-11-15 17:48:14 +0300

    Задача 3.3.1. Библиотека

---
+++ 3.3.1/src/Book.java
@@ -0,0 +1,14 @@
+import java.time.Year;
+
+/**
+ * @author Stanislav Rakitov
+ */
+public class Book {
+  private String author;
+  private String title;
+
+  public Book(String author, String title) {
+    this.author = author;
+    this.title = title;
+  }
+}
---
+++ 3.3.1/src/User.java
@@ -0,0 +1,10 @@
+/**
+ * @author Stanislav Rakitov
+ */
+public class User{
+
+  private String name;
+  private Book book;
+
+
+}
