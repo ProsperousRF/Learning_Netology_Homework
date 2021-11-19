281e8e7e2fcaf619fc917da96457c84b02e2ca5a
Stanislav Rakitov <prosperousrf@gmail.com>
2021-11-19 22:00:58 +0300

    3.4.1 Проверка доступа к ресурсу

--- 2.2.1/src/Main.java
+++ 2.2.1/src/Main.java
@@ -1,16 +1,48 @@
-/** @author Stanislav Rakitov */
+import java.util.Random;
+
 public class Main {
+
+  private final int SIZE = 2;
+  private final int[][] colors;
+
+  public Main() {
+    this.colors = new int[SIZE][SIZE];
+    matrixFill();
+  }
+
+  private void matrixFill() {
+    Random random = new Random();
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        // для случайных значений воспользуемся готовым решением из библиотеки java.util.Random
+        colors[i][j] = random.nextInt(256);
+      }
+    }
+  }
+
+  public void printMatrix(int[][] matrix) {
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        // %4d означает, что мы под каждый номер резервируем 4 знака
+        // (незанятые будут заполнены пробелами)
+        // таким образом, у нас получится ровная таблица
+        System.out.format("%4d", matrix[i][j]);
+      }
+      // Переход на следующую строку
+      System.out.println();
+    }
+  }
+
+  public int[][] rotateMatrix(int[][] matrix) {
+    // TODO: Matrix rotation
+    return new int[SIZE][SIZE];
+  }
 
   public static void main(String[] args) {
 
-    int SIZE = 8;
-
-    Matrix colors = new Matrix(SIZE);
-    colors.print();
-
-    System.out.println("---");
-
-    Matrix rotatedColors = new Matrix(colors);
-    rotatedColors.print();
+    Main main = new Main();
+    main.printMatrix(main.colors);
+    int[][] rotatedColors = main.rotateMatrix(main.colors);
+    main.printMatrix(rotatedColors);
   }
 }
