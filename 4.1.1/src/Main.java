b99c77346c89be49073523f6ddb69d4d79f8f9b8
Stanislav Rakitov <prosperousrf@gmail.com>
2021-11-24 23:28:03 +0300

    4.1.1 Main.java

---
+++ 4.1.1/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * @author Stanislav Rakitov
+ */
+
+public class Main {
+
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      while (true) {
+//        System.out.println("Введите название задачи (для завершения введите end)");
+
+      }
+    }
+  }
+}
